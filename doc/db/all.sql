68e3ae834631ba6d4286fa1f91b6ec445e295ce1
likuan <15215020796@163.com>
2020-07-07 21:34:30 +0800

    1.使用database关联数据库 2.建立数据库脚步

---
+++ doc/db/all.sql
@@ -0,0 +1,8 @@
+drop table if exists `test`;
+
+CREATE TABLE `test`  (
+                               `id` int(0) NOT NULL AUTO_INCREMENT,
+                               `name` varchar(255) NULL,
+                               PRIMARY KEY (`id`)
+);
+
