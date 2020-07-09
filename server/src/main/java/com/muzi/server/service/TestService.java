e9938057cfe30b4d273beeef02662128f4e74846
likuan <15215020796@163.com>
2020-07-09 20:58:42 +0800

    1.使用Example方法

--- server/src/main/java/com/muzi/server/service/TestService.java
+++ server/src/main/java/com/muzi/server/service/TestService.java
@@ -1,5 +1,9 @@
 package com.muzi.server.service;
 
+import com.muzi.server.entity.Test;
+import com.muzi.server.entity.TestExample;
+import com.muzi.server.mapper.TestMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -8,5 +12,14 @@ import java.util.List;
 @Service
 public class TestService {
 
+    @Resource
+    TestMapper testMapper;
+
+     List<Test>  list(){
+         TestExample testExample = new TestExample();
+         testExample.createCriteria().andIdEqualTo(1);
+         testExample.setOrderByClause("id desc");
+         return testMapper.selectByExample(testExample);
+     }
 
  }
